200688cb02ec54684cd2287a57129463c23bdeea
Luke Devonshire <6423205+LukeDevs@users.noreply.github.com>
2020-06-19 19:05:51 +0100

    Added additional outputs (#2)

--- output.tf
+++ output.tf
@@ -1,7 +1,15 @@
+output "names"{
+  value = local.rule_names
+}
+
+output "configurations" {
+  value = local.rule_configurations
+}
+
 output "deny_internet_inbound" {
-  value = local.predefined_rules.DenyInternetInBound
+  value = local.rule_configurations.DenyInternetInBound
 }
 
 output "deny_internet_outbound" {
-  value = local.predefined_rules.DenyInternetOutBound
+  value = local.rule_configurations.DenyInternetOutBound
 }
